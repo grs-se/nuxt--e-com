452e5b49392febf6fb93f502f34fb5cd38683065
grs-se <george_ricesmith@hotmail.com>
2023-03-22 21:45:23 +0000

    Dynamically render car detail page

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,9 +1,16 @@
 <script setup>
 const route = useRoute();
+const { cars } = useCars();
 const { toTitleCase } = useUtilities();
 
 useHead({
 	title: toTitleCase(route.params.name),
+});
+
+const car = computed(() => {
+	return cars.find((c) => {
+		return c.id === parseInt(route.params.id);
+	});
 });
 
 definePageMeta({
@@ -12,10 +19,10 @@ definePageMeta({
 </script>
 
 <template>
-	<div>
-		<CarDetailHero />
-		<CarDetailAttributes />
-		<CarDetailDescription />
+	<div v-if="car">
+		<CarDetailHero :car="car" />
+		<CarDetailAttributes :features="car.features" />
+		<CarDetailDescription :description="car.description" />
 		<CarDetailContact />
 	</div>
 </template>
